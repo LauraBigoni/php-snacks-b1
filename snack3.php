c10f1e7e4d5d62c202facec37bd65c872cbca350
Laura Bigoni <laurabigoni.92@gmail.com>
2022-02-25 18:16:39 +0100

    snack 3

--- snack3.php
+++ snack3.php
@@ -2,13 +2,28 @@
     Snack 3
 Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta. -->
 
-<?php 
+<?php
+$min = 1;
+$max = 50;
+$quantity = 15;
 
+// $numbers = range($min, $max);
+// var_dump($numbers);
+$rndNums = [];
+while (count($rndNums) < $quantity) {
+    do {
+        $rndNum = mt_rand(1, 100);
+        // var_dump($rndNum);
+    } while (in_array($rndNum, $rndNums));
+    $rndNums[] = $rndNum;
+}
+// var_dump($rndNums);
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -16,7 +31,7 @@ Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà co
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
-    <title>Document</title>
+    <title>Snack 3</title>
 </head>
 
 <style>
@@ -26,6 +41,13 @@ Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà co
 </style>
 
 <body>
-    
+    <h3>I numeri sono:</h3>
+    <ul>
+        <?php for ($i = 0; $i < count($rndNums); $i++) { ?>
+            <li> <?php echo $rndNums[$i] ?></li>
+        <?php } ?>
+    </ul>
+
 </body>
+
 </html>
